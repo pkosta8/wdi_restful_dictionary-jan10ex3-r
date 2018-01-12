ea4912d6d126c1ffcafcabdb8a692b474976fd3d
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-12 15:37:26 -0500

    id placeholder

--- app/controllers/enteries_controller.rb
+++
@@ -1,9 +0,0 @@
-class EnteriesController < ApplicationController
-
-  def index
-render :index
-  end
-
-
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,11 @@ resources :entries
 #            POST   /entries(.:format)          entries#create
 #  new_entry GET    /entries/new(.:format)      entries#new
 # edit_entry GET    /entries/:id/edit(.:format) entries#edit
-#      entry GET    /entries/:id(.:format)      entries#show
+#     entry GET    /entries/:id(.:format)      entries#show
 #            PATCH  /entries/:id(.:format)      entries#update
 #            PUT    /entries/:id(.:format)      entries#update
 #            DELETE /entries/:id(.:format)      entries#destroy
+
+
 
 end
